559c7b53b9fdd314eb9190442ae556e546744f19
Alpha <alphagma@gmail.com>
2012-08-11 20:31:02 -0500

    Initial import.

---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php
+    function alphasmanifesto_comment($comment, $args, $depth) {
+        $GLOBALS['comment'] = $comment;
+?>
+        <div class="comment row" id="comment-<?php echo comment_ID(); ?>">
+<?php
+        switch ($comment->comment_type) {
+            case 'pingback':
+            case 'trackback':
+?>
+            <div class="twocol"></div>
+            <div class="commentContent trackback eightcol">
+                <p><strong>Trackback:</strong> <?php comment_author_link(); ?></p>
+            </div>
+            <div class="twocol last"></div>
+<?php 
+                break;
+            default:
+?>
+            <div class="twocol"></div>
+            <div class="commentContent eightcol">
+                <div class="commentAuthor">
+                    <div class="commentAvatar"><?php echo get_avatar($comment);  ?></div>
+                    <p class="commentAuthorName"><?php echo get_comment_author_link(); ?></p>
+                    <p><?php echo get_comment_date(); ?></p>
+                    <p><?php echo get_comment_time(); ?></p>
+                </div>
+                <?php if ( $comment->comment_approved == '0' ) { ?>
+                    <p class="needModeration">El comentario está pendiente de aprobación.</p>
+                <?php } else {
+                    comment_text();
+                } ?>
+                <hr class="commentSeparator" />
+            </div>
+            <div class="twocol last"></div>
+<?php 
+        }
+?>
+        </div>
+<?php
+    }
+?>
---
+++ searchform.php
@@ -0,0 +1,14 @@
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <label for="s">Buscar:</label>
+    <input type="text" class="field" name="s" id="s" placeholder="Buscar (presiona Enter)" />
+</form>
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+        $('#searchform input').keyup(function(evt) {
+            if (event.which == 13) {
+                evt.preventDefault();
+                $('#searchform').submit();
+            }
+        });
+    });
+</script>
