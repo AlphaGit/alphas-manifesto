d766bd54fa8b2a95ed30dc9affdec774be110ec2
Alpha <alphagma@gmail.com>
2012-11-01 22:53:38 -0500

    Content-Type declaration before anything else.

--- single.php
+++ single.php
@@ -13,8 +13,8 @@ $content_width = 745;
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <title><?php echo $headTitle ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title><?php echo $headTitle ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
     <!--[if lte IE 9]><link rel="stylesheet" href="<?php bloginfo( 'stylesheet_directory' ); ?>/ie.css" type="text/css" media="screen" /><![endif]-->
