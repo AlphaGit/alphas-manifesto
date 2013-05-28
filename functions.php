e266840834ae37ddef761c2f0e2ddd2e1036e27b
Alpha <alphagma@gmail.com>
2013-05-27 21:47:58 -0500

    Displaying threaded comments. References #10.

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@
     function alphasmanifesto_comment($comment, $args, $depth) {
         $GLOBALS['comment'] = $comment;
 ?>
-        <article <?php comment_class("comment row"); ?> id="comment-<?php echo comment_ID(); ?>">
+        <article <?php comment_class("row"); ?> id="comment-<?php echo comment_ID(); ?>">
 <?php
         switch ($comment->comment_type) {
             case 'pingback':
@@ -36,17 +36,19 @@
 ?>
             <div class="twocol"></div>
             <div class="commentContent eightcol">
-                <div class="commentAuthor">
-                    <div class="commentAvatar"><?php echo get_avatar($comment);  ?></div>
-                    <p class="commentAuthorName"><?php echo get_comment_author_link(); ?></p>
-                    <p><?php echo get_comment_date(); ?></p>
-                    <time pubdate datetime="<?php echo get_comment_time("c") ?>"><?php echo get_comment_time(); ?></time>
+                <div class="commentContentContainer">
+                    <div class="commentAuthor">
+                        <div class="commentAvatar"><?php echo get_avatar($comment);  ?></div>
+                        <p class="commentAuthorName"><?php echo get_comment_author_link(); ?></p>
+                        <p><?php echo get_comment_date(); ?></p>
+                        <time pubdate datetime="<?php echo get_comment_time("c") ?>"><?php echo get_comment_time(); ?></time>
+                    </div>
+                    <?php if ( $comment->comment_approved == '0' ) { ?>
+                        <p class="needModeration">El comentario está pendiente de aprobación.</p>
+                    <?php } else {
+                        comment_text();
+                    } ?>
                 </div>
-                <?php if ( $comment->comment_approved == '0' ) { ?>
-                    <p class="needModeration">El comentario está pendiente de aprobación.</p>
-                <?php } else {
-                    comment_text();
-                } ?>
                 <hr class="commentSeparator" />
             </div>
             <div class="twocol last"></div>
