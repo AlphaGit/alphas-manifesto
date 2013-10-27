dce94d42d4d1299ebfd21b56836aeecce3ce9f62
Alpha <alphagma@gmail.com>
2013-10-26 20:54:55 -0500

    404 file

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php 
+    get_header(); 
+    $blogUrl = get_bloginfo('url');
+    $stylesheetDir = get_bloginfo( 'stylesheet_directory' );
+?>
+<div id="postsContainer" class="container">
+    <article id="" class="row">
+        <div class="metadata twocol"> </div>
+        <div class="eightcol postContent">
+            <h1>Oooops!</h1>
+            <h2 class="subtitle">Nada por aquí, nada por acá tampoco.</h2>
+            <p>No hay nada por aquí &ndash; ¿quizá lo que estabas buscando está en otro lado?</p>
+            <p>Puedes intentar <a href="<?php echo $blogUrl ?>">visitando la página principal</a>
+                o usando el siguiente formulario de búsqueda: </p>
+            <?php get_search_form(); ?>
+        </div>
+        <div class="twocol last"> </div>
+    </article>
+</div>
+<?php get_footer(); ?>
