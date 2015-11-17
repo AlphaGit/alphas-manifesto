5812ed4d598587ecd7a336cd8e149d09f948654d
Alpha <alphagma@gmail.com>
2015-11-17 05:42:52 -0600

    Fixed styles for 404 page

--- 404.php
+++ 404.php
@@ -1,25 +1,26 @@
-<?php
-    get_header();
-?>
-<div class="columnContainer">
-    <div class="column colSize1">
-        <?php wp_nav_menu(array(
-            'theme_location' => 'left-sidebar',
-            'container' => 'nav',
-            'container_class' => 'leftMenu'
-        )); ?>
+<?php get_header(); ?>
+    <div class="columnContainer">
+        <div class="column colSize1 leftSidebar">
+            <?php if(is_active_sidebar('left_sidebar')) {
+            ?>
+                <input type="checkbox" id="displayMenus" class="menuSwitcher" />
+                <label for="displayMenus" class="menuSwitcher"></label>
+                <?php dynamic_sidebar('left_sidebar');
+            } ?>
+        </div><!-- This comment is a fix for inline-block spaces, do not remove
+        --><div class="column colSize5 postContainer">
+            <article class="post columnContainer">
+                <div class="postContent column colSize4">
+                    <h1>Oooops!</h1>
+                    <p class="subtitle">Nothing over here. Neither over there.</p>
+                    <p>There's nothing over here &ndash; maybe what you were looking for is somewhere else?</p>
+                    <p>You can <a href="<?php echo home_url() ?>">try visiting the main page</a>
+                        or using the following search form:</p>
+                    <?php get_search_form(); ?>
+                </div>
+            </article>
+        </div>
     </div>
-    <div class="column colSize5">
-        <article class="post columnContainer">
-            <div class="postContent column colSize4">
-                <h1>Oooops!</h1>
-                <h2 class="subtitle">Nothing over here. Neither over there.</h2>
-                <p>There's nothing over here &ndash; maybe what you were looking for is somewhere else?</p>
-                <p>You can <a href="<?php echo home_url() ?>">try visiting the main page</a>
-                    or using the following search form:</p>
-                <?php get_search_form(); ?>
-            </div>
-        </article>
-    </div>
-</div>
-<?php get_footer(); ?>
+<?php wp_footer(); ?>
+</body>
+</html>
