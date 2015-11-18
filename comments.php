bfc9aca2a28d98bcbe7c931a80cf4085cb7ac5db
Alpha <alphagma@gmail.com>
2015-11-18 06:43:41 -0600

    Fixed comments not in a list

--- comments.php
+++ comments.php
@@ -5,14 +5,16 @@
         <div class="column colSize4">
             <h2>Comments</h2>
 
-            <?php if(have_comments()) {
-                wp_list_comments();
-                ?> <div class="commentPagination"> <?php
-                    paginate_comments_links();
-                ?> </div> <?php
-            } else {
-                ?> <p class="comments-no-comments">(There are currently no comments for this post.)</p> <?php
-            }
+            <?php if(have_comments()) { ?>
+                <ul class="commentList">
+                    <?php wp_list_comments(array('style' => 'ul')); ?>
+                </ul>
+                <div class="commentPagination">
+                    <?php paginate_comments_links(); ?>
+                </div>
+            <?php } else { ?>
+                <p class="comments-no-comments">(There are currently no comments for this post.)</p>
+            <?php }
 
             if (!comments_open()) {
                 ?> <p class="comments-disabled">(Comments are disabled for this post.)</p> <?php
