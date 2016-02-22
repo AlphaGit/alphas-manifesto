627a1817fb475ea37a8adc58b85bf4d042b54f94
Alpha <alphagma@gmail.com>
2016-02-22 16:11:52 -0600

    Avoided shadowing window.addEventListener

--- header.php
+++ header.php
@@ -37,66 +37,68 @@
     <?php add_action('wp_enqueue_scripts', 'alphasmanifesto_enqueue_scripts'); ?>
     <?php wp_head(); ?>
     <script type="text/javascript">
-        // helper functions, used to avoid jQuery as a dependency
-        function onBodyReady(fn) {
-            if (document.readyState != 'loading') {
-                fn();
-            } else if (document.addEventListener) { // IE9+
-                document.addEventListener('DOMContentLoaded', fn);
-            } else { // IE8
-                document.attachEvent('onreadystatechange', function() {
-                    if (document.readyState != 'loading') fn();
-                });
-            }
-        }
-
-        function addEventListener(el, eventName, handler) {
-          if (el.addEventListener) {
-            el.addEventListener(eventName, handler);
-          } else {
-            el.attachEvent('on' + eventName, function(){
-              handler.call(el);
-            });
-          }
-        }
-
-        // theme adjustments
-        function adjustEmbedSizes() {
-            var embeds = document.querySelectorAll('article iframe, article video, article embed');
-            for (var i = 0; i < embeds.length; i++) {
-                var embedded = embeds[i];
-                var declaredWidth = +embedded.getAttribute('width');
-                var declaredHeight = +embedded.getAttribute('height');
-                if (!!declaredHeight && !!declaredWidth) {
-                    embedded.removeAttr('width');
+        (function(document) {
+            // helper functions, used to avoid jQuery as a dependency
+            function onBodyReady(fn) {
+                if (document.readyState != 'loading') {
+                    fn();
+                } else if (document.addEventListener) { // IE9+
+                    document.addEventListener('DOMContentLoaded', fn);
+                } else { // IE8
+                    document.attachEvent('onreadystatechange', function() {
+                        if (document.readyState != 'loading') fn();
+                    });
                 }
             }
-        }
 
-        function searchParentsByTag(element, upperTagName) {
-            if (element.nodeName === upperTagName) return element;
-            if (element.nodeName === 'WINDOW') return null;
-            return searchParentsByTag(element.parentNode, upperTagName);
-        }
-
-        function searchBoxEventHandler(evt) {
-            if (evt.which == 13) {
-                evt.preventDefault();
-                var closesForm = searchParentsByTag(evt.target, 'FORM');
-                closesForm.submit();
-            }            
-        }
-
-        function bindEnterToSearchBox() {
-            var searchBoxes = document.querySelectorAll('.searchForm input.searchTerm');
-            for (var i = 0; i < searchBoxes.length; i++) {
-                var searchBox = searchBoxes[i];
-                addEventListener(searchBox, 'keyup', searchBoxEventHandler);
+            function addEventListener(el, eventName, handler) {
+              if (el.addEventListener) {
+                el.addEventListener(eventName, handler);
+              } else {
+                el.attachEvent('on' + eventName, function(){
+                  handler.call(el);
+                });
+              }
             }
-        }
 
-        onBodyReady(adjustEmbedSizes);
-        onBodyReady(bindEnterToSearchBox);
+            // theme adjustments
+            function adjustEmbedSizes() {
+                var embeds = document.querySelectorAll('article iframe, article video, article embed');
+                for (var i = 0; i < embeds.length; i++) {
+                    var embedded = embeds[i];
+                    var declaredWidth = +embedded.getAttribute('width');
+                    var declaredHeight = +embedded.getAttribute('height');
+                    if (!!declaredHeight && !!declaredWidth) {
+                        embedded.removeAttr('width');
+                    }
+                }
+            }
+
+            function searchParentsByTag(element, upperTagName) {
+                if (element.nodeName === upperTagName) return element;
+                if (element.nodeName === 'WINDOW') return null;
+                return searchParentsByTag(element.parentNode, upperTagName);
+            }
+
+            function searchBoxEventHandler(evt) {
+                if (evt.which == 13) {
+                    evt.preventDefault();
+                    var closesForm = searchParentsByTag(evt.target, 'FORM');
+                    closesForm.submit();
+                }            
+            }
+
+            function bindEnterToSearchBox() {
+                var searchBoxes = document.querySelectorAll('.searchForm input.searchTerm');
+                for (var i = 0; i < searchBoxes.length; i++) {
+                    var searchBox = searchBoxes[i];
+                    addEventListener(searchBox, 'keyup', searchBoxEventHandler);
+                }
+            }
+
+            onBodyReady(adjustEmbedSizes);
+            onBodyReady(bindEnterToSearchBox);
+        })(document);
     </script>
 </head>
 <body <?php body_class(); ?>>
