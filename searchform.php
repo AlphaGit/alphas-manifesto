c251a79d12ff3b74baeeeca4c16344c24eb2c79a
Alpha <alphagma@gmail.com>
2016-04-04 06:53:01 -0500

    Using esc_attr_x and est_attr_e for attributes

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form method="get" class="searchForm" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label for="s"><?php echo _x('Search:', 'verb', 'alphas-manifesto') ?></label>
-    <input type="text" class="searchTerm" name="s" id="s" placeholder="<?php echo __('Search term', 'alphas-manifesto') ?>" value="<?php the_search_query() ?>" />
-    <input type="submit" value="<?php echo _x('Search', 'verb', 'alphas-manifesto') ?>" class="searchSubmit" />
+    <input type="text" class="searchTerm" name="s" id="s" placeholder="<?php esc_attr_e('Search term', 'alphas-manifesto') ?>" value="<?php the_search_query() ?>" />
+    <input type="submit" value="<?php echo esc_attr_x('Search', 'verb', 'alphas-manifesto') ?>" class="searchSubmit" />
 </form>
